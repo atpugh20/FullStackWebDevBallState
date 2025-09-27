1cc5676b96c75533d725ea11cef674be0fcdb7a0
Alex <133706872+atpugh20@users.noreply.github.com>
2025-09-27 04:34:10 +0000

    feat: added api connection to the database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import { postsRoutes } from './routes/posts.js';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
+
+postsRoutes(app);
 
 app.get('/', (req, res) => {
   res.send(
