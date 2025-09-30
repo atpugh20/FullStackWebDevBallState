1585686e2a6579111686729022bd6c62dab3cf1e
Alex <133706872+atpugh20@users.noreply.github.com>
2025-09-30 04:46:29 +0000

    chore: connected the front end to the back end database

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,10 @@
-import { PostList } from "./components/PostList.jsx";
-import { CreatePost } from "./components/CreatePost.jsx";
-import { PostFilter } from "./components/PostFilter.jsx";
-import { PostSorting } from "./components/PostSorting.jsx";
-
-const posts = [
-    {
-        title: "Full-Stack React Projects",
-        contents: "Let's become full-stack developers!",
-        author: "Alex Pugh",
-    },
-    {
-        title: "Hello React!",
-        contents: "Perhaps?",
-        author: "Alex Pugh",
-    },
-];
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Blog } from "./Blog.jsx";
+const queryClient = new QueryClient();
 export function App() {
     return (
-        <div style={{ padding: 8 }}>
-            <CreatePost />
-            <br />
-            <hr />
-            Filter by:
-            <PostFilter field="author" />
-            <br />
-            <PostSorting fields={["createdAt", "updatedAt"]} />
-            <hr />
-            <PostList posts={posts} />
-        </div>
+        <QueryClientProvider client={queryClient}>
+            <Blog />
+        </QueryClientProvider>
     );
 }
-
-export default App;
