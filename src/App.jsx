d36c38e8824b186a3198ddbfb86a3159a0d89cf1
Alex <133706872+atpugh20@users.noreply.github.com>
2025-12-05 07:15:31 +0000

    chore: cleanup and integrate socket with react

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { Blog } from './pages/Blog.jsx'
 import { Signup } from './pages/Signup.jsx'
 import { Login } from './pages/Login.jsx'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
-import { io } from 'socket.io-client'
+// import { io } from 'socket.io-client'
+import { SocketIOContextProvider } from './contexts/SocketIOContext.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-const socket = io(import.meta.env.VITE_SOCKET_HOST, {
+/* const socket = io(import.meta.env.VITE_SOCKET_HOST, {
   query: 'room=' + new URLSearchParams(window.location.search).get('room'),
-})
-
+  auth: {
+    token: new URLSearchParams(window.location.search).get('token'),
+  },
+}) */
 const queryClient = new QueryClient()
 const router = createBrowserRouter([
   {
@@ -24,8 +27,7 @@ const router = createBrowserRouter([
     element: <Login />,
   },
 ])
-
-socket.on('connect', async () => {
+/* socket.on('connect', async () => {
   console.log('connected to socket.io as', socket.id)
   socket.emit(
     'chat.message',
@@ -39,14 +41,15 @@ socket.on('connect_error', (err) => {
 })
 socket.on('chat.message', (msg) => {
   console.log(`${msg.username}: ${msg.message}`)
-})
+}) */
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-        <RouterProvider router={router} />
+        <SocketIOContextProvider>
+          <RouterProvider router={router} />
+        </SocketIOContextProvider>
       </AuthContextProvider>
     </QueryClientProvider>
   )
 }
-
