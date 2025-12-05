caa0c1f21800153b3091b4b589b8ae25ad48e39d
Alex <133706872+atpugh20@users.noreply.github.com>
2025-12-05 07:27:30 +0000

    chore: start ui for chat

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,30 @@
-import { Link } from "react-router-dom";
-import { User } from "./User.jsx";
-import { jwtDecode } from "jwt-decode";
-import { useAuth } from "../contexts/AuthContext.jsx";
-
+import { Link } from 'react-router-dom'
+import { jwtDecode } from 'jwt-decode'
+import { User } from './User.jsx'
+import { useSocket } from '../contexts/SocketIOContext.jsx'
+import { useAuth } from '../contexts/AuthContext.jsx'
 export function Header() {
-    const [token, setToken] = useAuth();
-
-    if (token) {
-        const { sub } = jwtDecode(token);
-        return (
-            <div>
-                Logged in as <User id={sub} />
-                <br />
-                <button onClick={() => setToken(null)}>Logout</button>
-            </div>
-        );
-    }
-
+  const [token, setToken] = useAuth()
+  const { socket } = useSocket()
+  const handleLogout = () => {
+    socket.disconnect()
+    setToken(null)
+  }
+  if (token) {
+    const { sub } = jwtDecode(token)
     return (
-        <div>
-            <Link to="/login">Log In</Link> | <Link to="/signup">Sign Up</Link>
-        </div>
-    );
+      <div>
+        <h1>Welcome to My Blog!</h1>
+        Logged in as <User id={sub} />
+        <br />
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h1>Welcome to My Blog!</h1>
+      <Link to='/login'>Log In</Link> | <Link to='/signup'>Sign Up</Link>
+    </div>
+  )
 }
