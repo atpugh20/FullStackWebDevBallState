e1724fc75c202aae8a673ff93bc15d3b6e733243
Alex <133706872+atpugh20@users.noreply.github.com>
2025-12-06 08:45:40 +0000

    feat: post sort by most likes

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,29 @@
 import { Fragment } from "react";
 import PropTypes from "prop-types";
 import { Post } from "./Post.jsx";
-import { getDailyViews } from "../api/events.js";
+import { getTotalViews } from "../api/events.js";
+import { useQueries } from "@tanstack/react-query";
+
+let likeMap = {};
 
 export function PostList({ posts = [] }) {
+    const viewQueries = useQueries({
+        queries: posts.map((post) => ({
+            queryKey: ["totalViews", post.id],
+            queryFn: () => getTotalViews(post.id),
+        })),
+    });
+
+    const postsWithViews = posts.map((post, i) => ({
+        ...post,
+        views: viewQueries[i].data?.views ?? 0,
+    }));
+
+    const sorted = postsWithViews.sort((a, b) => b.views - a.views);
 
     return (
         <div>
-            {posts.map((post) => (
+            {sorted.map((post) => (
                 <Fragment key={post._id}>
                     <Post {...post} key={post._id} />
                     <hr />
